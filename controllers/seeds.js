1d36109637517ed0dd54c3a33319d7b6ea27a75c
crickett <crickettcode@gmail.com>
2018-04-12 13:50:25 -0400

    seeds created

--- controllers/seeds.js
+++ controllers/seeds.js
@@ -2,3 +2,39 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const Schema = require("../models/Schema");
+
+
+router.get('/Schema', function (req, res) {
+
+
+
+
+    var newPark = [
+
+        {
+            name: "old fourth",
+            level: "advanced",
+            img: "http://somelink",
+
+        }, {
+            name: "a.langford",
+            level: "beginner",
+            img: "http://somelink",
+        }, {
+            name: "brooke run",
+            level: "intermediate",
+            img: "http://somelink",
+        }, {
+            name: "Hazard County",
+            level: "all",
+            img: "http://somelink",
+        }
+
+
+    ];
+
+    Schema.create(newSchema, function (err) {
+        console.log("SEED: NEW PARKS CREATED");
+        res.redirect('/');
+
+        module.exports = router; 
