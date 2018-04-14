494ce69f4d69668cc5259c3c9b9e909a309ba933
crickett <crickettcode@gmail.com>
2018-04-14 14:15:36 -0400

    css works

--- app.js
+++ app.js
@@ -24,23 +24,26 @@ mongoose.connection.on('error', (error) => {
 
 const indexRouter = require('./routes/index');
 const parksRouter = require('./routes/parks');
+//app.use(express.static(path.join(__dirname, 'public')));
 
 const app = express();
-
-//view engine setup
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(methodOverride('_method'))
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/parks', parksRouter);
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 //\public
 
 // catch 404 and forward to error handler
