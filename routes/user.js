15bf716812293e71270c561e0b33be676ce46413
crickett <crickettcode@gmail.com>
2018-04-16 11:40:51 -0400

    user index

---
+++ routes/user.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router({ mergeParams: true })
+
+const Park = require('../models/Park')
+
+router.get('/', (request, response) => {
+    const parkId = request.params.parkId
+
+    Park.findById(parkId)
+        .then((park) => {
+            res.render('users/index', {
+
+            })
+        })
+})
