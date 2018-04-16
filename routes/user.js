fb25ba977dace16b53e2ab7b93f34b71fc0efced
crickett <crickettcode@gmail.com>
2018-04-16 12:09:41 -0400

    changes to seeds and edit

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const router = express.Router({ mergeParams: true })
+//const express = require('express')
+//const router = express.Router({ mergeParams: true })
 
-const Park = require('../models/Park')
+//const Park = require('../models/Park')
 
-router.get('/', (request, response) => {
-    const parkId = request.params.parkId
+//router.get('/', (request, response) => {
+    //const parkId = request.params.parkId
 
-    Park.findById(parkId)
-        .then((park) => {
-            res.render('users/index', {
+   // Park.findById(parkId)
+       // .then((park) => {
+         //   res.render('users/index', {
 
-            })
-        })
-})
+            //})
+       // })
+//})
